a9a909c521f0f0fb64400284735542be142c21ce
Samu Németh <nemeth.samu.0202@gmail.com>
2025-09-01 23:52:36 +0200

    Removed currently not present dependencies from Lf.

--- apps/lf/previewer.sh
+++ apps/lf/previewer.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 case "$1" in
-    *.pdf) pdftotext -l 5 "$1" -;;
+    # *.pdf) ls -l@ "$1";;
+    *.pdf) printf "\033[30;47;1mpdf";;
     *.png | *.jpg | *.jpeg | *.webp) printf "\033[30;47;1mImage";;
     *.mp4 | *.mkv) printf "\033[30;47;1mVideo";;
     *.mp3 | *.wav) printf "\033[30;47;1mImage";;
-    *) highlight -O ansi "$1";;
+    *) cat "$1";;
 esac
