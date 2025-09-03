6dcbe0e51fcd1afac4fb264887b3894bcc1791fe
Samu Németh <nemeth.samu.0202@gmail.com>
2025-09-03 20:54:00 +0200

    Added Checkstyle configuration for Java coding.

--- modules/programs/java.nix
+++ modules/programs/java.nix
@@ -14,6 +14,8 @@
   # --- Home Manager Part ---
   home-manager.users.${globals.user} = { config, pkgs, lib, ... }: {
 
+  home.file.".config/checkstyle_config.xml".source =
+    config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/System/apps/code/checkstyle_config.xml";
 
 
   };
