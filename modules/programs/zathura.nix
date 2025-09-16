78287cddda0e6192c13636f6988fd4433fb3c3dc
Samu Németh <nemeth.samu.0202@gmail.com>
2025-09-16 16:36:18 +0200

    Added recoloring to Zathura.

--- modules/programs/zathura.nix
+++ modules/programs/zathura.nix
@@ -1,6 +1,22 @@
 # --- Zathura ---
 
 { config, pkgs, lib, globals, ... }:
+let
+
+  # --- Color Settings ---
+  colors = {
+    background = {
+      main = "#14161B";
+      contrast = "#0A0B0E";
+    };
+    foreground = {
+      main = "#F2F4F3";
+      soft = "#D0D6DD";
+      error = "#DC4332";
+    };
+  };
+
+in
 {
 
   # --- Home Manager Part ---
@@ -13,30 +29,36 @@
 
     options = {
 
-# Configure status bar
+      # Configure status bar
       "guioptions" = "s";
-      "statusbar-bg" = "#00000000";
-      "statusbar-fg" = "#999999";
+      "statusbar-bg" = colors.background.contrast;
+      "statusbar-fg" = colors.foreground.main;
       "statusbar-basename" = "true";
 
-# Configure notifications
-      "notification-bg" = "#BBBBBB";
-      "notification-fg" = "#000000";
+      # Configure notifications
+      "notification-bg" = colors.foreground.soft;
+      "notification-fg" = colors.background.contrast;
 
-# Configure window title
+      # Configure window title
       "window-title-home-tilde" = "true";
 
-# Copy selection to system clipboard
+      # Copy selection to system clipboard
       "selection-clipboard" = "clipboard";
+
+      "recolor" = "true";
+      "recolor-keephue" = "true";
+      "recolor-reverse-video" = "false";
+      "recolor-lightcolor" = colors.background.main;
+      "recolor-darkcolor" = colors.foreground.main;
 
     };
 
     mappings = {
 
-# Aligns the page for navigations
+      # Aligns the page for navigations
       "zz" = "feedkeys \"<S-P>\"";
 
-# Maps navigations keys for smooth scrolling
+      # Maps navigations keys for smooth scrolling
       "j" = "feedkeys \"<C-Down>\"";
       "k" = "feedkeys \"<C-Up>\"";
       "h" = "feedkeys \"<C-Left>\"";
