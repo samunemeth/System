4f06b4b41f59d40f40c5cc29266738c076d4c30a
Samu Németh <nemeth.samu.0202@gmail.com>
2025-09-19 10:26:24 +0200

    Fixed mislabeling of audio files is lf.

--- apps/lf/previewer.sh
+++ apps/lf/previewer.sh
@@ -5,6 +5,6 @@ case "$1" in
     *.pdf) printf "\033[30;47;1mpdf";;
     *.png | *.jpg | *.jpeg | *.webp) printf "\033[30;47;1mImage";;
     *.mp4 | *.mkv) printf "\033[30;47;1mVideo";;
-    *.mp3 | *.wav) printf "\033[30;47;1mImage";;
+    *.mp3 | *.wav) printf "\033[30;47;1mAudio";;
     *) cat "$1";;
 esac
