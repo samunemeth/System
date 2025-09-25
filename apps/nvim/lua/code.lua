36a13dd0b87afbee01b1e0be467d008017923861
Samu Németh <nemeth.samu.0202@gmail.com>
2025-09-25 14:40:39 +0200

    Added shortcuts to Neovim for VSCode.

--- apps/nvim/lua/code.lua
+++ apps/nvim/lua/code.lua
@@ -4,5 +4,7 @@ if vim.g.vscode then
 
   vim.keymap.set("n", "<leader>cw", "<Cmd>lua require('vscode').call('editor.action.rename')<CR>")
   vim.keymap.set("n", "=", "<Cmd>lua require('vscode').call('editor.action.formatDocument')<CR>")
+  vim.keymap.set("n", "<leader>r", "<Cmd>lua require('vscode').call('workbench.action.debug.run')<CR>")
+  vim.keymap.set("n", "<leader>s", "<Cmd>lua require('vscode').call('workbench.action.debug.stop')<CR>")
 
 end
