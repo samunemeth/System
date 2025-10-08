3583e201a1947126bf15db7209f4caf438396626
Samu Németh <nemeth.samu.0202@gmail.com>
2025-10-08 10:29:07 +0200

    Added keybinding for formatting all files.

--- apps/nvim/after/plugin/conform.lua
+++ apps/nvim/after/plugin/conform.lua
@@ -11,6 +11,29 @@ if conform then
 	})
 
 	vim.keymap.set("n", "=", function()
-		conform.format()
+		conform.format({ async = true }, function(err, did_edit)
+			if err then
+				print(err)
+			else
+				if did_edit then
+					print("Formatted file.")
+				else
+					print("Nothing to format.")
+				end
+			end
+		end)
 	end)
 end
+
+vim.keymap.set("n", "<leader>=", function()
+	vim.fn.jobstart("treefmt", {
+		on_exit = function(job_id, exit_code)
+			if exit_code == 0 then
+				vim.cmd("checktime")
+				print("Formatted all saved files.")
+			else
+				print("There has been an error whlie formatting files in the project. Exit code: " .. exit_code)
+			end
+		end,
+	})
+end)
