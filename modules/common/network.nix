c4c381d32ae39283e65b882c9e6ae0e3737f56ee
Samu Németh <nemeth.samu.0202@gmail.com>
2025-10-13 15:20:43 +0200

    Added declarative enterprise networks.

--- modules/common/network.nix
+++ modules/common/network.nix
@@ -8,6 +8,76 @@
   ...
 }:
 let
+
+  # Define networks present in the secrets.
+  normal_networks = [
+    "HOME"
+    "GERARD"
+    "HOTSPOT"
+  ];
+  enterprise_networks = [
+    "TUE"
+  ];
+
+  # Generate configuration for the specified networks, if secrets are available.
+  normal_network_config = (
+    builtins.foldl' (
+      profiles: id:
+      profiles
+      // {
+        ${id} = {
+
+          connection.id = id;
+          wifi.ssid = "\$${id}_SSID";
+          wifi-security.key-mgmt = "\$${id}_MGMT";
+          wifi-security.psk = "\$${id}_PASS";
+
+          # Generic options that need to be set.
+          wifi.mode = "infrastructure";
+          wifi-security.auth-alg = "open";
+          connection.type = "wifi";
+
+          # Automatic IP address on all networks.
+          ipv4.method = "auto";
+          ipv6.method = "auto";
+          ipv6.addr-gen-mode = "default";
+
+        };
+      }
+    ) { } normal_networks
+  );
+
+  enterprise_network_config = (
+    builtins.foldl' (
+      profiles: id:
+      profiles
+      // {
+        ${id} = {
+
+          connection.id = id;
+          wifi.ssid = "\$${id}_SSID";
+          "802-1x".identity = "\$${id}_IDEN";
+          "802-1x".password = "\$${id}_PASS";
+
+          # Options for enterprise networks.
+          wifi-security.key-mgmt = "wpa-eap";
+          "802-1x".eap = "peap";
+          "802-1x".phase2-auth = "mschapv2";
+
+          # Generic options that need to be set.
+          wifi.mode = "infrastructure";
+          connection.type = "wifi";
+
+          # Automatic IP address on all networks.
+          ipv4.method = "auto";
+          ipv6.method = "auto";
+          ipv6.addr-gen-mode = "default";
+
+        };
+      }
+    ) { } enterprise_networks
+  );
+
 in
 {
 
@@ -34,38 +104,8 @@ in
     config.sops.secrets.wireless-env.path
   ];
 
-  # Add the profiles that are declared at the end form the sops file.
+  # Add normal and enterprise networks.
   networking.networkmanager.ensureProfiles.profiles =
-    builtins.foldl'
-      (
-        profiles: id:
-        profiles
-        // {
-          ${id} = {
-
-            connection.id = id;
-            wifi.ssid = "\$${id}_SSID";
-            wifi-security.key-mgmt = "\$${id}_MGMT";
-            wifi-security.psk = "\$${id}_PASS";
-
-            # Generic options that need to be set.
-            wifi.mode = "infrastructure";
-            wifi-security.auth-alg = "open";
-            connection.type = "wifi";
-
-            # Automatic IP address on all networks.
-            ipv4.method = "auto";
-            ipv6.method = "auto";
-            ipv6.addr-gen-mode = "default";
-
-          };
-        }
-      )
-      { }
-      [
-        "HOME"
-        "GERARD"
-        "HOTSPOT"
-      ];
+    normal_network_config // enterprise_network_config;
 
 }
