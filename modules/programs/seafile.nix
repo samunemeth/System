4bc19a06c6684270285c3070b0b9f962dcbeaee6
Samu Németh <nemeth.samu.0202@gmail.com>
2025-10-31 18:07:39 +0100

    Added automatic Seafile initialization.

--- modules/programs/seafile.nix
+++ modules/programs/seafile.nix
@@ -18,45 +18,79 @@
         Enables the seafile daemon for file scncing.
       '';
     };
-  };
-
-  config = lib.mkIf config.modules.seafile.enable {
-
-    environment.systemPackages = with pkgs; [
-      seafile-shared
-    ];
-
-    # Start the Seafile daemon automatically.
-    systemd.services.seafile-daemon = {
-      wantedBy = [ "multi-user.target" ];
-      path = [ pkgs.seafile-shared ];
-      script = ''
-        seaf-cli start
-      '';
-      serviceConfig = {
-        Type = "forking";
-        User = globals.user;
-      };
+    modules.seafile.repos = lib.mkOption {
+      type = lib.types.attrsOf lib.types.str;
+      default = {};
+      example = {};
+      description = '''';
     };
+  };
 
-    # --- Home Manager Part ---
-    home-manager.users.${globals.user} =
-      {
-        config,
-        pkgs,
-        lib,
-        ...
-      }:
-      {
+  config = lib.mkIf config.modules.seafile.enable (
+    let
 
-        # Create folder structure for Seafile.
-        home.file = {
-          ".ccnet/seafile.ini".text = "${config.home.homeDirectory}/.seafile-client/seafile-data";
-          ".seafile-client/seafile-data/.keep".text = "";
-        };
+      seafile-repos = config.modules.seafile.repos;
+      seafile-repoids =
+        "\"" + (builtins.concatStringsSep "\" \"" (builtins.attrNames seafile-repos)) + "\"";
+      seafile-repolocs =
+        "\"" + (builtins.concatStringsSep "\" \"" (builtins.attrValues seafile-repos)) + "\"";
 
+      userOwnedSecret = {
+        owner = globals.user;
+        group = "users";
       };
 
-  };
+    in
+    {
+
+      environment.systemPackages = with pkgs; [
+        seafile-shared
+      ];
+
+      # Request secrets for Seafile.
+      sops.secrets.seafile-url = userOwnedSecret;
+      sops.secrets.seafile-user = userOwnedSecret;
+      sops.secrets.seafile-pass = userOwnedSecret;
+
+      # Start the Seafile daemon automatically.
+      systemd.services.seafile-daemon = {
+        wantedBy = [ "multi-user.target" ];
+        path = [ pkgs.seafile-shared ];
+        script = ''
+
+          mkdir -p /home/${globals.user}/.seafile-client
+          seaf-cli init -d /home/${globals.user}/.seafile-client
+          seaf-cli start
+
+          repoids=(${seafile-repoids})
+          repolocs=(${seafile-repolocs})
+
+          remoteurl=$(< ${config.sops.secrets.seafile-url.path})
+          remoteuser=$(< ${config.sops.secrets.seafile-user.path})
+          remotepass=$(< ${config.sops.secrets.seafile-pass.path})
+
+          for i in "''${!repoids[@]}"; do
+            repoid="''${repoids[i]}"
+            repoloc="/home/${globals.user}/''${repolocs[i]}"
+            printf "Looking at %s with location %s\n" $repoid $repoloc
+            if seaf-cli list | grep -q "$repoid"; then
+              echo "$repoid already synced."
+            else
+              echo "Adding $repoid to sync."
+              mkdir -p "$repoloc"
+              seaf-cli sync -l "$repoid" -s "$remoteurl" -d "$repoloc" -u "$remoteuser" -p "$remotepass"
+            fi
+          done
+
+
+        '';
+        serviceConfig = {
+          Type = "forking";
+          User = globals.user;
+        };
+      };
+
+    }
+  );
 
 }
