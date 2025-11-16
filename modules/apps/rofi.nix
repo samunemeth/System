ae5085b3c24a592ab4ce822f9a2cd9b15504af3e
Samu Németh <nemeth.samu.0202@gmail.com>
2025-11-16 23:28:40 +0100

    Wrapped Rofi.

--- modules/apps/rofi.nix
+++ modules/apps/rofi.nix
@@ -7,73 +7,76 @@
   globals,
   ...
 }:
+let
+
+  wrapped-networkmanager-dmenu = pkgs.symlinkJoin {
+    name = "wrapped-networkmanager-dmenu";
+    buildInputs = [ pkgs.makeWrapper ];
+    paths = [ pkgs.networkmanager_dmenu ];
+    postBuild =
+      let
+        networkmanager-dmenu-config = pkgs.writers.writeText "networkmanager-dmenu-config.ini" ''
+          [dmenu]
+          dmenu_command = rofi -i
+          active_chars = ==
+          highlight = True
+          highlight_fg =
+          highlight_bg =
+          highlight_bold = True
+          compact = True
+          wifi_icons = 󰤯󰤟󰤢󰤥󰤨
+          format = {name:<20} {icon:<4} {sec} 
+          list_saved = False
+          prompt = Networks
+
+          [dmenu_passphrase]
+          obscure = False
+          obscure_color = ${globals.colors.background.main}
+
+          [editor]
+          terminal = alacritty
+          gui_if_available = False
+
+          [nmdm]
+          rescan_delay = 3
+        '';
+      in
+      ''
+        wrapProgram $out/bin/networkmanager_dmenu \
+          --add-flags "--config ${networkmanager-dmenu-config}"
+      '';
+  };
+
+  wrapped-rofi = pkgs.symlinkJoin {
+    name = "wrapped-rofi";
+    buildInputs = [ pkgs.makeWrapper ];
+    paths = [ pkgs.rofi ];
+    postBuild =
+      let
+        rofi-config = pkgs.writers.writeText "rofi-config.rasi" ''
+          configuration {
+            font: "Hack Nerd Font Mono 13";
+          }
+          @theme "${pkgs.rofi}/share/rofi/themes/Arc${if globals.colors.dark then "-Dark" else ""}.rasi"
+        '';
+      in
+      ''
+        wrapProgram $out/bin/rofi \
+          --add-flags "-config ${rofi-config}"
+      '';
+  };
+
+in
 {
 
   environment.systemPackages = with pkgs; [
 
-    rofi # Stylish Dmenu alternative.
-    rofi-calc # Calculator plugin for Rofi.
-    networkmanager_dmenu # Network manager plugin for Rofi.
+    wrapped-rofi # Simple command running interface with configuration.
+    wrapped-networkmanager-dmenu # Network manager plugin for Rofi with configuration.
 
   ];
 
-  # --- Home Manager Part ---
-  home-manager.users.${globals.user} =
-    {
-      config,
-      pkgs,
-      lib,
-      ...
-    }:
-    {
+  # Require fonts used.
+  fonts.packages = [ pkgs.nerd-fonts.hack ];
 
-      programs.rofi = {
-
-        enable = true;
-
-        package = pkgs.rofi;
-        font = "Hack Nerd Font Mono 13";
-        theme =
-          if globals.colors.dark then
-            "${pkgs.rofi}/share/rofi/themes/Arc-Dark.rasi"
-          else
-            "${pkgs.rofi}/share/rofi/themes/Arc.rasi";
-
-        plugins = with pkgs; [
-          rofi-calc
-          rofi-power-menu
-        ];
-
-      };
-
-      # Configuration for network manager dmenu.
-      home.file.".config/networkmanager-dmenu/config.ini".text = ''
-
-        [dmenu]
-        dmenu_command = rofi -i
-        active_chars = ==
-        highlight = True
-        highlight_fg =
-        highlight_bg =
-        highlight_bold = True
-        compact = True
-        wifi_icons = 󰤯󰤟󰤢󰤥󰤨
-        format = {name:<20} {icon:<4} {sec} 
-        list_saved = False
-        prompt = Networks
-
-        [dmenu_passphrase]
-        obscure = False
-        obscure_color = ${globals.colors.background.main}
-
-        [editor]
-        terminal = alacritty
-        gui_if_available = False
-
-        [nmdm]
-        rescan_delay = 5
-
-      '';
-
-    };
 }
