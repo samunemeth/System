50f24a70ac1ef429ab2f820f6bd936830e5d221d
Samu Németh <nemeth.samu.0202@gmail.com>
2025-11-27 20:40:47 +0100

    added Bitwarden Firefox extension

--- modules/apps/firefox.nix
+++ modules/apps/firefox.nix
@@ -78,6 +78,12 @@
               installation_mode = "force_installed";
             };
 
+            # Bitwarden
+            "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
+              install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
+              installation_mode = "force_installed";
+            };
+
           };
 
           # Preferences that control the browser behaviour.
