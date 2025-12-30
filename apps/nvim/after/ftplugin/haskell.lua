1b8f4e3d8bb3b47ca6d086f7fe946c17cf4efa36
Samu Németh <nemeth.samu.0202@gmail.com>
2025-12-30 23:22:30 +0100

    made sure safe require is used as intended in Neovim configuration

--- apps/nvim/after/ftplugin/haskell.lua
+++ apps/nvim/after/ftplugin/haskell.lua
@@ -9,5 +9,7 @@ local ht = safe_require("haskell-tools")
 local bufnr = vim.api.nvim_get_current_buf()
 local buffspec = { buffer = bufnr }
 
-vim.keymap.set("n", "<space>r", ht.lsp.buf_eval_all, buffspec)
-vim.keymap.set('n', '<space>a', vim.lsp.codelens.run, buffspec)
+if ht then
+	vim.keymap.set("n", "<space>r", ht.lsp.buf_eval_all, buffspec)
+	vim.keymap.set("n", "<space>a", vim.lsp.codelens.run, buffspec)
+end
