df60e30e10512aeda4ed37fd1572c59742841fb3
Samu Németh <nemeth.samu.0202@gmail.com>
2026-01-11 00:46:22 +0100

    Neovim: added a markdown table formatter.

--- apps/nvim/after/ftplugin/markdown.lua
+++ apps/nvim/after/ftplugin/markdown.lua
@@ -4,3 +4,12 @@ vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
+
+-- This formats a markdown table using a single shell command.
+-- Please keep a safe distance from this abomination.
+-- Touch and comprehend at your own risk!
+vim.keymap.set(
+	"x",
+	"<leader>t",
+	[[:'<,'>! sed 's/|\([^-: ]\)/| \1/g' | tr -s ' ' | column -t -s '|' -o '|' | sed '2{s/ /-/g; s/:\(-\+\)-|/-\1:|/g}'<CR>]]
+)
