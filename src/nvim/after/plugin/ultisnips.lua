00719b5124f6c478b1787c5fcaa09dbaf9f500b5
Samu Németh <nemeth.samu.0202@gmail.com>
2026-01-24 23:16:40 +0100

    neovim: fixed duplicate UltiSnips configuration

--- src/nvim/after/plugin/ultisnips.lua
+++
@@ -1,5 +0,0 @@
---- Configuration for UltiSnips ---
-
--- Make UltiSnips user the declarative directories.
-vim.g.UltiSnipsSnippetDirectories = { "UltiSnips" }
-vim.g.UltiSnipsSnippetsDir = "/tmp"
--- src/nvim/lua/before.lua
+++ src/nvim/lua/before.lua
@@ -1,12 +1,7 @@
---- For script that need to load before plugins ---
+-- For script that need to load before plugins.
 
--- UltiSnips
--- This configuration needs to load before the plugin does.
--- NOTE: In the future, I may need to switch to LuaSnips
+--- UltiSnips ---
 vim.g.UltiSnipsExpandOrJumpTrigger = "<Tab>"
 vim.g.UltiSnipsJumpBackwardTrigger = "<S-Tab>"
-vim.g.UltiSnipsSnippetDirectories = {
-	-- WARN: This should probably not depend on the users home directory if the
-	-- > configuration files are later wrapped with Neovim.
-	vim.fn.expand("$HOME/.config/nvim/UltiSnips"),
-}
+vim.g.UltiSnipsSnippetDirectories = { "UltiSnips" }
+vim.g.UltiSnipsSnippetsDir = "/tmp"
