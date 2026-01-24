d2599982acb911a90841ed5af653fd4f8a33da6a
Samu Németh <nemeth.samu.0202@gmail.com>
2026-01-25 00:27:25 +0100

    fixed formatting

--- src/nvim/after/ftplugin/tex.lua
+++ src/nvim/after/ftplugin/tex.lua
@@ -9,13 +9,13 @@ if knap then
 		buffer = 0,
 		callback = function()
 			knap.process_once()
-      vim.defer_fn(function()
-        knap.forward_jump()
-      end, 500)
+			vim.defer_fn(function()
+				knap.forward_jump()
+			end, 500)
 		end,
 	})
 
-  -- Do a jump in Zathura to the current location.
+	-- Do a jump in Zathura to the current location.
 	vim.keymap.set({ "n", "v" }, "<leader>a", function()
 		knap.forward_jump()
 	end)
